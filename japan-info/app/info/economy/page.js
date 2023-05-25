1d92123dba7c5d11d266405dc51fef0d45ce3f9d
Felix De Haro <felixddhs@outlook.com>
2023-05-24 18:49:23 -0600

    economy ready

--- japan-info/app/info/economy/page.js
+++ japan-info/app/info/economy/page.js
@@ -10,41 +10,41 @@ import {
 const features = [
     {
         name: 'Main Exports.',
-        description: 'Refined petroleum, packaged medicaments, aluminum plating, fresh fish, and fruit (particularly oranges, olives, and apricots)\n',
+        description: 'Refined petroleum, packaged medicines, aluminum plating, cotton, cheese, copper piping (2021)',
         icon: GlobeAsiaAustraliaIcon,
     },
     {
         name: 'Main Imports.',
-        description: 'Crude petroleum, packaged medicaments, cars, refined petroleum, and computers\n',
+        description: 'Crude petroleum, refined petroleum, packaged medicines, cars, ships (2019)',
         icon: GlobeAmericasIcon,
     },
     {
         name: 'Import Partners.',
-        description: 'China 24%, United States 10%, Australia 7%, South Korea 4%, Taiwan 4% (2021)',
+        description: 'Germany 11%, China 9%, Italy 8%, Iraq 7%, Russia 6%, Netherlands 5% (2019)',
         icon: BanknotesIcon,
     },
     {
         name: 'Export Partners.',
-        description: 'China 21%, United States 18%, South Korea 7%, Taiwan 7%, Thailand 4% (2021)',
+        description: 'Italy 10%, Germany 7%, Turkey 5%, Cyprus 5%, Bulgaria 5% (2019)',
         icon: BanknotesIcon,
     },
 ]
 
 const economicSectorFeatures = [
     {
-        name: 'Technology.',
+        name: 'Services.',
         description:
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+            'Most of the greek service income comes from tourism. Greece is the 7th most visited country in the EU. Tourism accounts for 20% of the GDP and employs 20% of the workforce.',
         icon: CloudArrowUpIcon,
     },
     {
         name: 'Industry.',
-        description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+        description: 'It includes food and tobacco processing, textiles, chemicals, metal products, mining, and petroleum',
         icon: LockClosedIcon,
     },
     {
         name: 'Agriculture.',
-        description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+        description: 'Maize, olives, wheat, milk, peaches/nectarines, oranges, tomatoes, grapes, milk, potatoes',
         icon: ServerIcon,
     },
 ]
@@ -65,10 +65,10 @@ export default function Economia(){
         <div className="py-24 lg:pt-12 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8 ">
                 <div className="mx-auto max-w-2xl sm:text-center">
-                    <h2 className="text-base font-semibold leading-7 text-red-600">Japanese</h2>
+                    <h2 className="text-base font-semibold leading-7 text-blue-600">Greek</h2>
                     <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Economy</p>
                     <p className="mt-6 text-lg leading-8 text-gray-600">
-                        It&apos;s the fourth largest economies in the world, with a trade oriented an diversified industrial base.
+                        Tourism and shipping-based EU economy; clientelism economic culture and systemic corruption is the current state of the greek economy.
                     </p>
                 </div>
             </div>
@@ -105,7 +105,7 @@ export default function Economia(){
                     {features.map((feature) => (
                         <div key={feature.name} className="relative pl-9">
                             <dt className="inline font-semibold text-gray-900">
-                                <feature.icon className="absolute left-1 top-1 h-5 w-5 text-red-600" aria-hidden="true" />
+                                <feature.icon className="absolute left-1 top-1 h-5 w-5 text-blue-600" aria-hidden="true" />
                                 {feature.name}
                             </dt>{' '}
                             <dd className="inline">{feature.description}</dd>
@@ -117,17 +117,16 @@ export default function Economia(){
                 <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                     <div className="lg:ml-auto lg:pl-4 lg:pt-4">
                         <div className="lg:max-w-lg">
-                            <h2 className="text-base font-semibold leading-7 text-red-600">Production</h2>
+                            <h2 className="text-base font-semibold leading-7 text-blue-600">Production</h2>
                             <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Economic Sectors</p>
                             <p className="mt-6 text-lg leading-8 text-gray-600">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-                                iste dolor cupiditate blanditiis ratione.
+                                These are the main economic sectors of the greek economy.
                             </p>
                             <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                                 {economicSectorFeatures.map((ecoFeature) => (
                                     <div key={ecoFeature.name} className="relative pl-9">
                                         <dt className="inline font-semibold text-gray-900">
-                                            <ecoFeature.icon className="absolute left-1 top-1 h-5 w-5 text-red-600" aria-hidden="true" />
+                                            <ecoFeature.icon className="absolute left-1 top-1 h-5 w-5 text-blue-600" aria-hidden="true" />
                                             {ecoFeature.name}
                                         </dt>{' '}
                                         <dd className="inline">{ecoFeature.description}</dd>
