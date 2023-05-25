36579c4193042ac807ed5f31716309ec82bce2d2
Felix De Haro <felixddhs@outlook.com>
2023-05-24 19:31:02 -0600

    finished hot topic

--- japan-info/app/info/hot-topic/page.js
+++ japan-info/app/info/hot-topic/page.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
 import Image from "next/image";
 
 export default function HotTopic() {
@@ -19,55 +18,37 @@ export default function HotTopic() {
                 </div>
                 <div className="px-6 lg:contents">
                     <div className="mx-auto max-w-2xl pb-24 pt-16 sm:pb-32 sm:pt-20 lg:ml-8 lg:mr-0 lg:w-full lg:max-w-lg lg:flex-none lg:pt-32 xl:w-1/2">
-                        <p className="text-base font-semibold leading-7 text-red-600">Hot news</p>
-                        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</h1>
+                        <p className="text-base font-semibold leading-7 text-blue-600">Hot news</p>
+                        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Political Change</h1>
                         <p className="mt-6 text-xl leading-8 text-gray-700">
-                            Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget
-                            aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget. Eleifend
-                            egestas fringilla sapien.
+                            A hot topic in Greece right now is the political situation.
                         </p>
                         <div className="mt-10 max-w-xl text-base leading-7 text-gray-700 lg:max-w-none">
                             <p>
-                                Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet
-                                vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque
-                                erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris
-                                semper sed amet vitae sed turpis id.
+                                After a recent election, Prime Minister Kyriakos Mitsotakis
+                                center-right New Democracy party dominated but failed to secure an outright majority in Parliament,
+                                leading to the decision to hold another election as early as June 25th. The party won double the votes
+                                of the left-wing Syriza party and nearly four times those of the third-place Socialist Pasok, but a one-off electoral law in place
+                                during the election denied them a governing majority.
                             </p>
-                            <ul role="list" className="mt-8 space-y-8 text-gray-600">
-                                <li className="flex gap-x-3">
-                                    <CloudArrowUpIcon className="mt-1 h-5 w-5 flex-none text-red-600" aria-hidden="true" />
-                                    <span>
-                    <strong className="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet
-                    consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
-                    blanditiis ratione.
-                  </span>
-                                </li>
-                                <li className="flex gap-x-3">
-                                    <LockClosedIcon className="mt-1 h-5 w-5 flex-none text-red-600" aria-hidden="true" />
-                                    <span>
-                    <strong className="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua
-                    ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
-                  </span>
-                                </li>
-                                <li className="flex gap-x-3">
-                                    <ServerIcon className="mt-1 h-5 w-5 flex-none text-red-600" aria-hidden="true" />
-                                    <span>
-                    <strong className="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien
-                    vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
-                  </span>
-                                </li>
-                            </ul>
                             <p className="mt-8">
-                                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor
-                                fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac
-                                adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.
+                                With 99.70% of the votes counted, New Democracy had 40.79% and 146 seats, five short of a majority,
+                                winning in 58 of the country&apos;s 59 constituencies. Syriza received 20.07% of the votes and 71 seats,
+                                while Pasok came in third with 11.46% of the vote. The turnout was 61%.
                             </p>
-                            <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
+                            <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">International Reaction.</h2>
                             <p className="mt-6">
-                                Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh.
-                                Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
-                                tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam
-                                turpis ipsum eu a sed convallis diam.
+                                This situation has been welcomed by markets as it seems to signal the end of the political uncertainty
+                                that troubled the NATO and European Union member following the 2009-18 financial crisis. The Athens stock
+                                exchange general index surged more than 7% and Greek bonds rallied.
+                            </p>
+                            <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">Conclusion.</h2>
+                            <p className="mt-6">
+                                Mitsotakis, 55, met with President Katerina Sakellaropoulou, who formally gave him the mandate to try to
+                                form a government, but he declared there would be no point as he saw no way for the current Parliament to form a
+                                government. He indicated that he would return the mandate so that new elections could be held as soon as possible.
+                                Mitsotakis had long suggested that he would not seek a coalition partner, whatever the election outcome, in the
+                                interest of stable governance.
                             </p>
                         </div>
                     </div>
